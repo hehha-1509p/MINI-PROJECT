95cc44d2645acf5ed9ba964820028af770559502
Teh Khai Xuan <goldteh2407@gmail.com>
2026-04-28 16:29:19 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DietController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DietController;
 
 /*
 |--------------------------------------------------------------------------
